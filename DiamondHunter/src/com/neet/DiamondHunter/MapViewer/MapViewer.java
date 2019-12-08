79b7d6062aa27c3d9c7f19349a187ff298aa0a1e
zhiying038 <zhiying038@gmail.com>
2019-12-08 22:54:17 +0800

    launch and initial structure of user interface

---
+++ DiamondHunter/src/com/neet/DiamondHunter/MapViewer/MapViewer.java
@@ -0,0 +1,34 @@
+package com.neet.DiamondHunter.MapViewer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class MapViewer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane page = FXMLLoader.load(getClass().getResource("MapViewer.fxml"));
+        Scene scene = new Scene(page);
+        scene.getStylesheets().add(getClass().getResource("MapViewer.css").toExternalForm());
+        primaryStage.setTitle("MapViewer");
+        primaryStage.getIcons().add(new Image("/FXML-Image/windowsIcon.png"));
+        primaryStage.setTitle(" MapViewer");
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.getIcons().add(new Image("/FXML-Image/windowsIcon.png"));
+
+    }
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
